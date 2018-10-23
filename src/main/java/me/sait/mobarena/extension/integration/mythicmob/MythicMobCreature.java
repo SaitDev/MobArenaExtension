9a9506c655a2f61dbe056c25574d98da204836ab
SaitDev <sait.glacialdevil@gmail.com>
2018-10-23 14:51:36 +0700

    can not support non-living entity mythic mob for now

--- src/main/java/me/sait/mobarena/extension/integration/mythicmob/MythicMobCreature.java
+++ src/main/java/me/sait/mobarena/extension/integration/mythicmob/MythicMobCreature.java
@@ -7,22 +7,34 @@ import io.lumine.xikage.mythicmobs.api.exceptions.InvalidMobTypeException;
 import io.lumine.xikage.mythicmobs.mobs.MythicMob;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 
 public class MythicMobCreature extends MACreature {
+    private MythicMobsSupport mythicMobsSupport;
     private MythicMob mythicMob;
 
-    public MythicMobCreature(MythicMob mythicMob) {
-        super(mythicMob.getInternalName().toLowerCase(), EntityType.fromName(mythicMob.getEntityType()));
+    public MythicMobCreature(MythicMobsSupport mythicMobsSupport, MythicMob mythicMob) {
+        super(mythicMob.getInternalName().toLowerCase().replaceAll("[-_\\.]",""),
+                EntityType.fromName(mythicMob.getEntityType()));
+
+        this.mythicMobsSupport = mythicMobsSupport;
         this.mythicMob = mythicMob;
     }
 
     @Override
     public LivingEntity spawn(Arena arena, World world, Location location) {
         try {
-            return (LivingEntity) MythicMobs.inst().getAPIHelper().spawnMythicMob(mythicMob, location, 0);
-        } catch (InvalidMobTypeException e) { //most likely should not happen
+            Entity mMob = MythicMobs.inst().getAPIHelper().spawnMythicMob(mythicMob, location, 0);
+            if (mMob instanceof LivingEntity) {
+                mythicMobsSupport.arenaSpawnMythicMob(arena, mMob);
+                return (LivingEntity)mMob;
+            } else {
+                return null;
+            }
+        } catch (InvalidMobTypeException e) {
+            //mythic mobs were reload but ma creatures can not be unregistered for compatible
             return null;
         }
     }
